5383170357a93adca773133050bcb0805b1a5317
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-06-08 12:08:28 +0200

    basic ts usage 2.2

--- 2_basic_ts_usage/src/demo.ts
+++ 2_basic_ts_usage/src/demo.ts
@@ -1,7 +1,20 @@
-let x: number 
-let y: string
-let z: boolean
-let a: Date
-let b: string[]
+interface Contact extends Address {
+    id: number;
+    name: string;
+    birthDate?: Date; 
+}
 
-b = "Hello!" as any
+interface Address {
+    line1: string;
+    line2: string;
+    province: string;
+    region: string;
+    postalCode: string;
+}
+
+let primaryContact: Contact = {
+    birthDate: new Date("01-01-1980"),
+    id: 12345,
+    name: "Jamie Johnson", 
+    postalCode:
+}
