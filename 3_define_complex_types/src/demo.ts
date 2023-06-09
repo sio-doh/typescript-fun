e8f6a76c126a77ffd7810045854b7be801a6921d
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-06-09 12:49:42 +0200

    define more complex types 3.5

--- 3_define_complex_types/src/demo.ts
+++ 3_define_complex_types/src/demo.ts
@@ -1,3 +1,11 @@
+let x: Record<string, string | number | boolean | Function> = { name: "Wruce Bayne" };
+x.number = 1234; 
+x.active = true;
+x.log = () => console.log("awesome!")
+x = 1234;
+
+///////////////////
+
 type ContactStatus = "active" | "inactive" | "new";
 
 interface Address {
@@ -12,37 +20,29 @@ interface Contact {
     address: Address;
 }
 
-type Awesome = Contact["address"]["postalCode"];
-
-interface ContactEvent {
-    contactId: Contact["id"];
+interface Query {
+    sort?: "asc" | "desc";
+    matches(val): boolean;
 }
 
-interface ContactDeletedEvent extends ContactEvent {
+function searchContacts(contacts: Contact[], query: Record<keyof Contact, Query>) {
+    return contacts.filter(contact => {
+        for (const property of Object.keys(contact) as (keyof Contact)[]) {
+            // get the query object for this property
+            const propertyQuery = query[property];
+            // check to see if it matches
+            if (propertyQuery && propertyQuery.matches(contact[property])) {
+                return true;
+            }
+        }
+        return false;
+    })
 }
 
-interface ContactStatusChangedEvent extends ContactEvent {
-    oldStatus: Contact["id"];
-    newStatus: Contact["id"];
-}
-
-interface ContactEvents {
-    deleted: ContactDeletedEvent;
-    statusChanged: ContactStatusChangedEvent;
-    // ... and so on
-}
-
-function getValue<T, U extends keyof T>(source: T, propertyName: U) {
-    return source[propertyName];
-}
-
-function handleEvent<T extends keyof ContactEvents>(
-    eventName: T, 
-    handler: (evt: ContactEvents[T]) => void 
-) {
-    if (eventName === "statusChanged") {
-        handler({ contactId: 1, oldStatus: "active", newStatus: "inactive" })
+const filteredContacts = searchContacts(
+    [/* contacts */],
+    {
+        id: { matches: (id) => id === 123 },
+        name: { matches: (name) => name === "Carol Weaver"},
     }
-}
-
-handleEvent("statusChanged", evt => evt)
+);
